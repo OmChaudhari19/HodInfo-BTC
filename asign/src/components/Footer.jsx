590760498db712f3b3b71bd41a89a6973de12fb8
OmChaudhari19~ <omchaudhari1904@gmail.com>
2024-01-24 15:57:14 +0530

    Final Complete

--- asign/src/components/Footer.jsx
+++ asign/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <div class=" bg-gray-900 ">
-      <div class="max-w-2xl mx-auto text-white py-10">
+    <div class=" bg-gray-900 relative">
+      <div class="max-w-2xl mx-auto text-white py-10 mt-40 bottom-0">
         <div class="flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
           <p class="order-2 md:order-1 mt-8 md:mt-0"> &copy; Om Chaudhari</p>
           <div class="order-1 md:order-2">
--- asign/src/components/Navbar.js
+++ asign/src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Row from "./Row";
-
+import wazir from "../wazirx.png"
 const Navbar = () => {
 
     const [count, setCount] = useState(1);
@@ -27,6 +26,7 @@ const Navbar = () => {
             setData(newdata);
         }
         fetch1()
+
     }, [])
 
 
@@ -62,8 +62,6 @@ const Navbar = () => {
 
 
 
-
-
     return (
         <>
             <div className='bg-gray-900 w-full h-40 flex justify-around'>
@@ -94,8 +92,8 @@ const Navbar = () => {
                         <button className='ml-10 mt-8 bg-gray-600 w-14 h-6 rounded-md'>BTC</button>
                     </div>
                 </div>
-                <div class=" w-20  rounded-full flex justify-center items-center border-white border-1 rounded-full">
-                    <p className='text-cyan-500 text-2xl pt-3 ml-14'>{count}</p>
+                <div class=" w-20  rounded-full flex justify-center items-center border-white border-1 rounded-full text-cyan-500">
+                    {count}
                 </div>
                 <div className='grow justify-center'>
                     <button className='bg-cyan-500 text-white mt-16 ml-12 h-10 w-44 rounded'>Connect Telegram</button>
@@ -103,11 +101,11 @@ const Navbar = () => {
             </div>
             <div className="stat bg-gray-900 ">
                 <ul className=' flex justify-around flex-end items-center'>
-                    <li className='px-14'><div className='text-center '><p className='text-cyan-500 text-3xl'>0.76%</p><p className='text-gray-400 text-xl'>5 min</p></div></li>
-                    <li className='px-14'><div className='text-center'><p className='text-cyan-500 text-3xl'>1.46%</p><p className='text-gray-400 text-xl'>1 Hour</p></div></li>
-                    <li className='px-14'><div className='text-center'><p className='text-center text-white text-7xl'>Rs 35,79,115</p><p className='text-gray-400 text-xl'>Average BTC/INR net price including commission</p></div></li>
-                    <li className='px-14'><div className='text-center'><p className='text-cyan-500 text-3xl'>8.35%</p><p className='text-gray-400 text-xl'>1 Day</p></div></li>
-                    <li className='px-14'><div className='text-center'><p className='text-cyan-500 text-3xl'>12.28%</p><p className='text-gray-400 text-xl'>7 Days</p></div></li>
+                    <li className='px-14'><div className='text-center pt-10'><p className='text-cyan-500 text-3xl'>0.76%</p><p className='text-gray-400 text-xl'>5 min</p></div></li>
+                    <li className='px-14'><div className='text-center pt-10'><p className='text-cyan-500 text-3xl'>1.46%</p><p className='text-gray-400 text-xl'>1 Hour</p></div></li>
+                    <li className='px-14'><div className='text-center'><p className='text-gray-400 text-3xl pb-4'>Best Place to Trade</p><p className='text-center text-white text-7xl'>Rs 35,79,115</p><p className='text-gray-400 text-xl'>Average BTC/INR net price including commission</p></div></li>
+                    <li className='px-14'><div className='text-center pt-10'><p className='text-cyan-500 text-3xl'>8.35%</p><p className='text-gray-400 text-xl'>1 Day</p></div></li>
+                    <li className='px-14'><div className='text-center pt-10'><p className='text-cyan-500 text-3xl'>12.28%</p><p className='text-gray-400 text-xl'>7 Days</p></div></li>
                 </ul>
             </div>
             <div class=" bg-gray-900 relative flex flex-col w-full  overflow-scroll text-black bg-black text-white shadow-md bg-clip-border scrollbar-hide pt-20 gettab">
@@ -152,17 +150,18 @@ const Navbar = () => {
                                 <tr>
                                     <td class="p-6 ml-6 border-b border-blue-gray-50">
                                         <p className="block font-sans text-xl antialiased font-normal leading-normal text-blue-gray-900">
-                                            {idex+1}
+                                            {idex + 1}
                                         </p>
                                     </td>
                                     <td class="p-6 ml-6 border-b border-blue-gray-50">
-                                        <p className="block font-sans text-xl antialiased font-normal leading-normal text-blue-gray-900"
+                                        <div className="block font-sans text-xl antialiased font-normal leading-normal text-blue-gray-900 flex"
                                         >
-                                            WazirX
-                                        </p>
+                                            <img src={wazir} alt="wazir-logo" className='h-8  pr-5 rounded-3xl' />
+                                            <p>WazirX</p>
+                                        </div>
                                     </td>
                                     <td class="p-6 ml-6 border-b border-blue-gray-50">
-                                        <p className="block font-sans text-xl antialiased font-normal leading-normal text-blue-gray-900"
+                                        <p className="block font-sans text-xl antialiased font-normal leading-normal text-blue-gray-900 pl-7"
                                         >
                                             {`Rs ${lastPrice}`}
                                         </p>
@@ -175,16 +174,16 @@ const Navbar = () => {
                                         </p>
                                     </td>
                                     <td class="p-6 ml-6 border-b border-blue-gray-50">
-                                        <p className="block font-sans text-xl antialiased font-medium leading-normal text-blue-gray-900"
+                                        <p className="block font-sans text-xl antialiased font-medium leading-normal text-blue-gray-900 pl-8"
                                         >
-                                            {`${((highPrice -lowPrice) / highPrice * 100).toFixed(2)} %`}
+                                            {`${((highPrice - lowPrice) / highPrice * 100).toFixed(2)} %`}
                                         </p>
                                     </td>
                                     <td class="p-6 ml-6 border-b border-blue-gray-50">
                                         <p
                                             className="block font-sans text-xl antialiased font-medium leading-normal text-blue-gray-900"
                                         >
-                                            {`Rs ${(highPrice- lowPrice).toFixed(2)}`}
+                                            {`Rs ${(highPrice - lowPrice).toFixed(2)}`}
                                         </p>
                                     </td>
                                 </tr>
